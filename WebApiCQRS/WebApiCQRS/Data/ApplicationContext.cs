7f00960ad31eba8944285a35c17f1e08cfd0e012
Manik <Captaiblood@users.noreply.github.com>
2023-02-20 09:38:25 +0000

    CQRS with Mediator
    Web API project implementing basic  CQRS with Mediator Lib.

---
+++ WebApiCQRS/WebApiCQRS/Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCQRS.Models;
+
+namespace WebApiCQRS.Data
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public async Task<int> SaveChnages()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ WebApiCQRS/WebApiCQRS/Data/IApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCQRS.Models;
+
+namespace WebApiCQRS.Data
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChnages();
+    }
+}
